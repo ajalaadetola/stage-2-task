dae211719756da6becdc5df2aab31f738244f1a6
ADETOLA AJALA <ajalaadetola7@gmail.com>
2025-10-30 19:32:21 +0100

    Update entrypoint.sh

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -15,7 +15,7 @@ export BACKUP_POOL
 echo "BACKUP_POOL set to: $BACKUP_POOL"
 
 # Substitute environment variables
-envsubst '${PORT},${ACTIVE_POOL},${BACKUP_POOL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${PORT},${ACTIVE_POOL} ${BACKUP_POOL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 echo "Nginx configuration completed:"
 echo "Active: app_${ACTIVE_POOL}:3000"
